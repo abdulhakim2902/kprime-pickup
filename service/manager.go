e531e9da3231606569c1b52f6480a16c7cf21c67
Abdul Hakim <hakim@undercurrent.tech>
2023-06-14 10:57:52 +0700

    handle activities when order is rejected

--- service/manager.go
+++ service/manager.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"pickup/datasources/collector"
 	"pickup/datasources/kafka"
+	"sync"
 	"time"
 
 	"git.devucc.name/dependencies/utilities/commons/log"
@@ -28,6 +29,8 @@ type ManagerService struct {
 	activityRepository interfaces.Repository[activity.Activity]
 	orderRepository    interfaces.Repository[order.Order]
 	tradeRepository    interfaces.Repository[trade.Trade]
+	nonce              int64
+	mutex              *sync.Mutex
 }
 
 func NewManagerService(
@@ -36,30 +39,36 @@ func NewManagerService(
 	o interfaces.Repository[order.Order],
 	t interfaces.Repository[trade.Trade],
 ) ManagerService {
-	return ManagerService{kafkaConn: k, activityRepository: a, orderRepository: o, tradeRepository: t}
+	n := int64(0)
+	p := []bson.M{{"$sort": bson.M{"createdAt": -1}}, {"$limit": 1}}
+	acts := a.Aggregate(p)
+	if len(acts) > 0 {
+		n = acts[0].Nonce
+	}
+
+	return ManagerService{
+		kafkaConn:          k,
+		activityRepository: a,
+		orderRepository:    o,
+		tradeRepository:    t,
+		nonce:              n,
+		mutex:              &sync.Mutex{},
+	}
 }
 
 func (m *ManagerService) HandlePickup(msg kafkago.Message) error {
-	// Get latest activity
-	activity := &activity.Activity{ID: primitive.NewObjectID(), Nonce: 0, CreatedAt: time.Now()}
-	pipeline := []bson.M{{"$sort": bson.M{"createdAt": -1}}, {"$limit": 1}}
-	activities := m.activityRepository.Aggregate(pipeline)
-
-	// Reassign nonce from mongodb
-	if len(activities) > 0 {
-		activity = activities[0]
-	}
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 
 	// Metrics
-	activity.ID = primitive.NewObjectID()
-	go collector.ConsumedMetricCounter(msg.Topic, activity.ID.Hex())
+	activityId := primitive.NewObjectID()
+	go collector.ConsumedMetricCounter(msg.Topic, activityId.Hex())
 
 	// Initialize data
 	orders := []*order.Order{}
 	trades := []*trade.Trade{}
 
 	nonce := int64(0)
-	mongoNonce := activity.Nonce + 1
 
 	var data interface{}
 	var er *engine.EngineResponse
@@ -70,7 +79,7 @@ func (m *ManagerService) HandlePickup(msg kafkago.Message) error {
 	switch msg.Topic {
 	case string(types.ENGINE):
 		er, nonce, err = m.processEngine(msg.Value)
-		orders, trades = m.validateOrders(er)
+		orders, trades, err = m.validateOrders(er)
 		data = m.activityData(msg.Topic, er)
 	case string(types.CANCELLED_ORDER):
 		co, nonce, err = m.processCancelledOrders(msg.Value)
@@ -79,43 +88,38 @@ func (m *ManagerService) HandlePickup(msg kafkago.Message) error {
 	}
 
 	if err != nil {
-		go collector.PublishedMetricCounter(msg.Topic, activity.ID.Hex(), false)
+		go m.kafkaConn.Commit(msg)
+		go collector.PublishedMetricCounter(msg.Topic, activityId.Hex(), false)
 		return nil
 	}
 
-	if nonce != mongoNonce {
-		logs.Log.Error().Msg("Invalid nonce!")
-		return m.manager(msg.Topic, activity.ID.Hex())
+	if nonce != m.nonce+1 {
+		i := map[string]interface{}{"engine": nonce, "mongo": m.nonce + 1}
+		logs.Log.Error().Any("nonce", i).Msg("Invalid nonce!")
+		return m.manager(msg.Topic, activityId.Hex())
 	}
 
-	// Update activity detail
-	activity.Nonce = mongoNonce
-	activity.Data = data
-	activity.CreatedAt = time.Now()
-
 	if err := m.updateOrders(orders); err != nil {
-		return m.manager(msg.Topic, activity.ID.Hex())
+		return m.manager(msg.Topic, activityId.Hex())
 	}
 
 	if err := m.updateTrades(trades); err != nil {
-		return m.manager(msg.Topic, activity.ID.Hex())
+		return m.manager(msg.Topic, activityId.Hex())
 	}
 
 	if err := m.kafkaConn.Commit(msg); err != nil {
-		return m.manager(msg.Topic, activity.ID.Hex())
+		return m.manager(msg.Topic, activityId.Hex())
 	}
 
 	if err := m.publishSaved(msg); err != nil {
-		return m.manager(msg.Topic, activity.ID.Hex())
+		return m.manager(msg.Topic, activityId.Hex())
 	}
 
-	filter := bson.M{"_id": activity.ID}
-	update := bson.M{"$set": activity}
-	if _, err := m.activityRepository.FindAndModify(filter, update); err != nil {
-		return m.manager(msg.Topic, activity.ID.Hex())
+	if err := m.insertActivity(activityId, data); err != nil {
+		return m.manager(msg.Topic, activityId.Hex())
 	}
 
-	go collector.PublishedMetricCounter(msg.Topic, activity.ID.Hex(), true)
+	go collector.PublishedMetricCounter(msg.Topic, activityId.Hex(), true)
 
 	return nil
 }
@@ -218,16 +222,16 @@ func (m *ManagerService) updateTrades(t []*trade.Trade) error {
 	return nil
 }
 
-func (m *ManagerService) validateOrders(e *engine.EngineResponse) (orders []*order.Order, trades []*trade.Trade) {
+func (m *ManagerService) validateOrders(e *engine.EngineResponse) (orders []*order.Order, trades []*trade.Trade, err error) {
 	orders = []*order.Order{}
 	trades = []*trade.Trade{}
 
 	if e == nil {
-		return orders, trades
+		return nil, nil, errors.New("EngineNotFound")
 	}
 
 	if e.Status == types.ORDER_REJECTED {
-		return orders, trades
+		return nil, nil, errors.New("OrderRejected")
 	}
 
 	if len(e.Matches.MakerOrders) > 0 {
@@ -242,17 +246,7 @@ func (m *ManagerService) validateOrders(e *engine.EngineResponse) (orders []*ord
 		orders = append(orders, e.Matches.TakerOrder)
 	}
 
-	return orders, trades
-}
-
-func (m *ManagerService) cancelledOrders(co *order.CancelledOrder) (orders []*order.Order) {
-	orders = []*order.Order{}
-
-	if co == nil {
-		return orders
-	}
-
-	return co.Data
+	return orders, trades, nil
 }
 
 func (m *ManagerService) publishSaved(msg kafkago.Message) error {
@@ -264,6 +258,20 @@ func (m *ManagerService) publishSaved(msg kafkago.Message) error {
 	default:
 		return errors.New("TopicNotFound")
 	}
+}
+
+func (m *ManagerService) insertActivity(id primitive.ObjectID, data interface{}) error {
+	activity := &activity.Activity{ID: id, Nonce: m.nonce + 1, Data: data, CreatedAt: time.Now()}
+
+	filter := bson.M{"_id": activity.ID}
+	update := bson.M{"$set": activity}
+	if _, err := m.activityRepository.FindAndModify(filter, update); err != nil {
+		return err
+	}
+
+	m.nonce += 1
+
+	return nil
 }
 
 func (m *ManagerService) manager(topic string, key string) error {
